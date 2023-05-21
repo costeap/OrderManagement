228cc6fa6bde644b97d5b85c2353cb7c2b73256c
Paula Costea <99597702+costeap@users.noreply.github.com>
2023-05-21 15:16:00 +0300

    Update ConnectionFactory.java

--- src/main/java/Conexiune/ConnectionFactory.java
+++ src/main/java/Conexiune/ConnectionFactory.java
@@ -14,7 +14,7 @@ public class ConnectionFactory {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String DBURL = "jdbc:mysql://localhost:3306/schooldb?autoReconnect=true&useSSL=false";
     private static final String USER = "root";
-    private static final String PASS = "Carambola23*";
+    private static final String PASS = "************";
 
     private static ConnectionFactory singleInstance = new ConnectionFactory();
 
